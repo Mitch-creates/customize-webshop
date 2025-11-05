b88a990f2749f3efea1f7641c9911182298fab39
Mitch-creates <mitchcreates@proton.me>
2025-11-05 22:08:22 +0100

    Fix for Prisma not finding .env variables

---
+++ app/api/users/route.ts
@@ -0,0 +1,13 @@
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+
+// const prisma = new PrismaClient();
+
+// export default async function handle(res: NextResponse) {
+//   const users = await prisma.user.findMany({
+//     where: {
+//       published: true,
+//     },
+//   });
+//   res.json(users);
+// }
--- auth.ts
+++ auth.ts
@@ -1,11 +1,13 @@
-// import { betterAuth } from "better-auth";
-// import { prismaAdapter } from "better-auth/adapters/prisma";
-// // If your Prisma file is located elsewhere, you can change the path
-// import { PrismaClient } from "@/generated/prisma/client";
+import { config } from "dotenv";
+config();
 
-// const prisma = new PrismaClient();
-// export const auth = betterAuth({
-//   database: prismaAdapter(prisma, {
-//     provider: "postgresql",
-//   }),
-// });
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});

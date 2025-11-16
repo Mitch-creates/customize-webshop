8a184b1e13664c892d9d92e94ca3e4bbc8569eeb
Mitch-creates <mitchcreates@proton.me>
2025-11-16 20:28:22 +0100

    Implement user session handling in Navbar; add dropdown menu for authenticated users

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,10 +1,25 @@
+"use client";
 import Link from "next/link";
 import CtaButton from "./cta-button";
 import RegularButton from "./regular-button";
 import { useTranslations } from "next-intl";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function Navbar() {
   const navigationMessages = useTranslations("navigation");
+  const session = authClient.useSession();
+  const router = useRouter();
+
+  const user = session.data?.user;
+
   return (
     <nav className="flex justify-between items-center p-4 border-b-2 select-none">
       <h1>
@@ -18,18 +33,42 @@ export default function Navbar() {
           <Link href="/Dashboard">{navigationMessages("about")}</Link>
         </li>
       </ul>
-      <ul className="flex space-x-4">
-        <li>
-          <RegularButton>{navigationMessages("signIn")}</RegularButton>
-        </li>
-        <li>
-          <Link href="/account/signup">
-            <CtaButton accent="color2">
-              {navigationMessages("signUp")}
-            </CtaButton>
-          </Link>
-        </li>
-      </ul>
+      {user && !session.isPending ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                authClient.signOut({
+                  fetchOptions: {
+                    onSuccess: () => {
+                      router.push("/");
+                    },
+                  },
+                });
+              }}
+            >
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <ul className="flex space-x-4">
+          <li>
+            <RegularButton>{navigationMessages("signIn")}</RegularButton>
+          </li>
+          <li>
+            <Link href="/account/signup">
+              <CtaButton accent="color2">
+                {navigationMessages("signUp")}
+              </CtaButton>
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
